8c8725f9e40e567a9dad11679817782e0e6e15fb
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-24 17:00:19 -0400

    still making the start game function

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ let highScore = document.getElementById('hiSco');
 let start = document.getElementById('start');
 let intro = document.getElementsByClassName('intros');
 let timer = 60;
+let score = 0;
+let questions = document.getElementById('question');
+
 
 function countdown() {
     let interval = setInterval(function() {
@@ -20,9 +23,30 @@ start.addEventListener('click', startGame)
   function startGame() {
     countdown();
     intro[0].style.display = 'none';
-    showQuestions();
+    // showQuestions();
     }
 
+// function showQuestions() {
+//   questions.innerHTML = '';
+//   for (let i = 0; i < question.length; i++) {
+
+ 
+// }
+
+// function checkAnswer() {
+  //}
+
+ let question = [
+    {
+        question: 'What is the capital of California?',
+        answers: [
+            {text: 'Sacramento', correct: true},
+            {text: 'Los Angeles', correct: false},
+            {text: 'San Francisco', correct: false},
+            {text: 'San Diego', correct: false}
+        ] 
+    },
+  ];
 
 
 
