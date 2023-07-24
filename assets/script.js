efbd9905690bd8d540400608067beb3343213429
Justin Delgado <delgadojustin100@yahoo.com>
2023-07-24 14:46:12 -0400

    starting the timer

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,18 @@ let time = document.getElementById('times');
 let highScore = document.getElementById('hiSco');
 let start = document.getElementById('start');
 
-time.innerHTML = 0;
+function countdown() {
+    let timer = 60;
+    let interval = setInterval(function() {
+        if (timer > 1) {
+           time.textContent = timer + ' seconds remaining';
+        } else if (timer === 1) {
+            time.textContent = timer + ' second remaining';
+        } else {
+            time.textContent = '';
+            clearInterval(interval);
+            alert('Game over');
+        } 
+    }, 1000);
+        }
+start.addEventListener('click', countdown) 
