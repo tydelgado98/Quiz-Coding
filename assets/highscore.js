4f41ea9d83672cdeb4431974f8d330cdf3de5dcf
Justin Delgado <delgadojustin100@yahoo.com>
2023-08-07 17:48:54 -0400

    making the local storage work

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,27 +1,27 @@
-// function showScores() {
-//     let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+function showScores() {
+    let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
     
-//     // Separate scores
-//     highscores.sort(function(a, b) {
-//         return b.score - a.score;
-//     });
+    // Separate scores
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
 
-//     let olList = document.getElementById("new-score-list");
-//     olList.innerHTML = ''; // Clear previous scores before showing updated ones
+    let olList = document.getElementById("new-score-list");
+    olList.innerHTML = ''; // Clear previous scores before showing updated ones
 
-//     for (let i = 0; i < highscores.length; i += 1) {
-//         let liEl = document.createElement('li');
-//         liEl.textContent = highscores[i].initials + '-' + highscores[i].score;
-//         olList.appendChild(liEl);
-//     }
-// }
+    for (let i = 0; i < highscores.length; i += 1) {
+        let liEl = document.createElement('li');
+        liEl.textContent = highscores[i].initials + '-' + highscores[i].score;
+        olList.appendChild(liEl);
+    }
+}
 
-// // Erase scores
-// function eraseHighscores() {
-//     window.localStorage.removeItem("highscores");
-//     window.location.reload();
-// }
+// Erase scores
+function eraseHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+}
 
-// document.getElementById("erase-new").onclick = eraseHighscores;
+document.getElementById("erase-new").onclick = eraseHighscores;
 
-// showScores();
+showScores();
